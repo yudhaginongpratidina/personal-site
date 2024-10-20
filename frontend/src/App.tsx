87c4460d2742c37facd067cdb5903ea4e0270886
yudhaginongpratidina <dev.yudhaginongpratidina.com>
2024-10-20 17:34:07 +0700

    add package setup react-router-dom and setup routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
+// LIBS
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// GLOBAL CSS
 import './styles/index.css'
 
+// PAGES
+import HomePage from './pages/HomePage'
+
+// ROOT
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/*" element={<HomePage />} />
+            </Routes>
+        </BrowserRouter>
     </StrictMode>,
 )
