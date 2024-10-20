fdff2fc4445b5a27f8056d498577f080a260e3e3
yudhaginongpratidina <dev.yudhaginongpratidina.com>
2024-10-20 17:15:02 +0700

    react-personal-site

--- frontend/src/partials/Footer/Index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer({children}: {children: React.ReactNode}){
-    return (
-        <nav className="w-full px-6 box-border border-t-2 border-blue-500 bg-white shadow-md select-none">
-            <div className="w-full h-12 container flex items-center justify-between">
-                { children }
-            </div>
-        </nav>
-    )
-}
--- frontend/src/partials/Navbar/Index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar({children}: {children: React.ReactNode}) {
-    return (
-        <nav className="w-full px-6 box-border border-b-2 border-blue-500 bg-white shadow-md select-none">
-            <div className="w-full h-14 container flex items-center justify-between">
-                { children }
-            </div>
-        </nav>
-    )
-}
